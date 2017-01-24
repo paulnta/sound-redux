ebe5b64bfed875b000c693f0fe32652c19b60dc4
paulnta <paul.ntawuruhunga@heig-vd.ch>
2017-01-24 14:49:51 +0100

    added redux-dev-tools

--- scripts/store/configureStore.js
+++ scripts/store/configureStore.js
@@ -1,11 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
-
 export default function configureStore(initialState) {
-  const store = createStoreWithMiddleware(rootReducer, initialState);
-
+  //noinspection JSUnresolvedVariable
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = createStore(rootReducer, initialState, composeEnhancers(
+    applyMiddleware(thunkMiddleware)
+  ))
   return store;
 }
